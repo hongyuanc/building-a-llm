fc592533f041b1c84cd220dfc9ce4cbc5a31b095
Hong <hong.cao0824@gmail.com>
2024-06-24 10:31:43 +0800

    cleaning code

--- bigram.py
+++ bigram.py
@@ -40,7 +40,7 @@ num_emb = 32
 # %%
 # bringing in tiktoken's tokenizer
 
-# THIS WILL NOT WORK
+# THIS DiD NOT WORK
 
 enc = tiktoken.get_encoding("gpt2")
 enc.n_vocab
@@ -144,11 +144,18 @@ class BigramLM(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, num_emb)
+        self.position_embedding_table = nn.Embedding(block_size, num_emb)
         self.lm_head = nn.Linear(num_emb, vocab_size)
 
     def forward(self, inputs, targets=None):
+        B,T = inputs.shape
+
         token_emb = self.token_embedding_table(inputs) # batch, time, channel
-        logits = self.lm_head(token_emb)
+        pos_emb = self.position_embedding_table(torch.arrange(T))
+
+        # x holds positions which tokens occur
+        x = token_emb + pos_emb
+        logits = self.lm_head(x)
 
         if targets is None:
             loss = None
@@ -175,10 +182,6 @@ class BigramLM(nn.Module):
         return inputs
     
 
-# %% [markdown]
-# Usually we would expect a loss of -ln(1/86), which is approximately **-4.45**, 
-# but we are getting almost 5 right now. This means the inital predictions are not very diffused yet, and there is entropy.
-
 # %%
 # this was where i found that tiktoken wouldn't work
 model = BigramLM()
@@ -188,21 +191,11 @@ logits, loss = model(xb, yb)
 print(logits.shape)
 print(loss)
 
-# %% [markdown]
-# This looks silly now because history is not used; we only examine the last character in time: **logits = logits[:, -1, :]**
-
-# %%
-# setting an input as a 1 by 1 tensor of zeros
-input = torch.zeros((1, 1), dtype=torch.long)
-print(decode(model.generate(input, 100)[0].tolist()))
 
 # %%
 # making a pytorch optimzer object
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
-# %% [markdown]
-# **Training the model.**
-# We can see the loss going down everytime we train it.
 
 # %%
 # increasing batch size and setting a loop to evaluate loss
@@ -222,18 +215,6 @@ for i in range(max_iters): # my computer almost blew up
 print(loss.item())
 #print(f"step {i}: train loss: {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
-# %% [markdown]
-# As we can see, there are drastic improvements than to before.
-# 
-# 
-# "—;84c7.nd]ontxe$4neHLKyéxUa—fW]’g0IySCPjoM3j,4R 0h—$é ],Oé4x$érBXJHP12MGay8?W’O[ ?[3xIV9é?*NC “va1G" 
-# 
-# to
-# 
-# 
-# " d I wnt harsurthed is owemeveg wayouio id aly a this wicy’se  d Minghe Shouad yoofezzive rnan y dineng o 
-# thiniss mecegs wayth inn r  “Antoke!” s ge, motthof, ay wes blut cemomos. astr I wa dad Wes dyecakneitlastat ceche. “
-# Ale nidosef My I roufol amed cousy ay?”  s, od. son’tanonghat athad Routheed d".
 
 # %%
 input = torch.zeros((1, 1), dtype=torch.long)
